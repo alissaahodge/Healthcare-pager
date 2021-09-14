a2891c0516dc6a1c1ce1fa3828c7c85e4c4b0227
Alissa <alissaahodge@gmail.com>
2021-09-14 18:02:57 -0400

    Added styles and sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 
 import {StreamChat} from "stream-chat";
 import {Chat} from "stream-chat-react";
-import Cookies from 'universal-cookie';
 
 import {ChannelListContainer, ChannelContainer} from "./components";
-import { API_KEY} from "../environment/environment";
+import { API_KEY} from "./environment/environment";
+
+import './App.css';
 
 const client = StreamChat.getInstance(API_KEY);
 
 const App = () => {
 return(<div className="app__wrapper">
     <Chat client={client} theme="team light">
-        <ChannelListContainer/>
-        <ChannelContainer/>
+        <ChannelListContainer
+        />
+        <ChannelContainer
+        />
     </Chat>
 </div>);
 };
